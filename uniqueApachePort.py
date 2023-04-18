03b4abb855a40c063398b09eabb928d03ce0f493
bridyboo <57050319+bridyboo@users.noreply.github.com>
2023-04-18 01:22:13 -0700

    Made some stylistic changes for the output on main

--- uniqueApachePort.py
+++ uniqueApachePort.py
@@ -1,5 +1,8 @@
 # Need to write exceptions try, catch, etc.
 # Another bug found, if the exception is caught I think it closes out the window immediately could be wrong tho
+# I think needs a function 'add multiple newPorts'
+# Regardless I need to figure out how to keep track of all the new ports, either saved in a separate log, or saved in
+# instance of the class as a global static variable (will look into it)
 # should be assigned to APACHE port XXXX1 if available
 import portCompare
 
@@ -8,8 +11,9 @@ result = portCompare.lookupPort(query)
 sortedResult = sorted(result)
 
 print("List of all ports that start with ", query)
+print("LocalPort \tStatus\n------\t\t------")
 for index, ports in enumerate(sortedResult):
-    print(sortedResult[index])
+    print(sortedResult[index], "\t\tListen")
 
 print("\nthis is the new unique port: ", portCompare.newPort(sortedResult))
 input()

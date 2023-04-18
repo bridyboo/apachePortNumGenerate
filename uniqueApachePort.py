3a0959a65c72a2486c9d2318a8326fea7bc8d8f7
bridyboo <57050319+bridyboo@users.noreply.github.com>
2023-04-18 01:41:29 -0700

    added use warning at the end

--- uniqueApachePort.py
+++ uniqueApachePort.py
@@ -15,5 +15,12 @@ print("LocalPort \tStatus\n------\t\t------")
 for index, ports in enumerate(sortedResult):
     print(sortedResult[index], "\t\tListen")
 
-print("\nthis is the new unique port: ", portCompare.newPort(sortedResult))
+result = portCompare.newPort(sortedResult) # Final result this is the unique Port
+print("\nthis is the new unique port: ", result)
+print("\n\n*warning if using with TD this will only query the current state of the server, this means\n"
+      "*until TEST has finished deploying port", str(result), ",this will always be the 'unique port' for this server\n"
+      "*I will look further for a solution in the future for now there's still some 'manual' work needed\n"
+      "-Matthew Brian Darmadi")
+
+print("\npress 'enter' to exit...")
 input()

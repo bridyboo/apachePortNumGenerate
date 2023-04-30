1ddb22a60b2bea91164e5a3e9d41ca340a4122b1
bridyboo <57050319+bridyboo@users.noreply.github.com>
2023-04-29 22:12:53 -0700

    updated client-side so clients can request another port lookup

--- uniqueApachePort.py
+++ uniqueApachePort.py
@@ -5,31 +5,30 @@
 # should be assigned to APACHE port XXXX1 if available
 import portCompare
 
-query = input("Please enter the first 2 digit of lookup port: ")
-result = portCompare.lookupPort(query)
+while True:
+    query = input("Please enter the first 2 digit of lookup port: ")
+    result = portCompare.lookupPort(query)
 
-print("List of all ports that start with ", query)
-print("LocalPort \tStatus\n------\t\t------")
-for index, ports in enumerate(result):
-    print(result[index], "\t\tListen")
+    print("List of all ports that start with ", query)
+    print("LocalPort \tStatus\n------\t\t------")
+    for index, ports in enumerate(result):
+        print(result[index], "\t\tListen")
 
-print("\nPress 'enter' to generate new ports, press 'n' to end the program")
-print("These the new unique ports: ")
-count = 0
-while True:  # generate multiple ports maybe
-    result = portCompare.generateNewPort(result)  # Final result this is the unique Port
-    print(portCompare.newPort(result))
+    print("\nPress 'enter' to generate new ports, press 'n' to end the program")
+    print("These the new unique ports: ")
+    count = 0
+    while True:  # generate multiple ports maybe
+        result = portCompare.generateNewPort(result)  # Final result this is the unique Port
+        print(portCompare.newPort(result))
+        user_input = input()
+        if user_input == 'n':
+            count += 1
+            break
+
+    print("\npress 'e' to exit...")
+    print("press 'enter' to lookup Ports...")
     user_input = input()
-    if user_input == 'n':
-        count += 1
+    if user_input == 'e':
         break
 
-
-print("\n\n*warning if using with TD this will only query the current state of the server, this means\n"
-      "*until TEST has finished deploying port", str(result),
-      ",this will always be the 'unique port' for this server\n"
-      "*I will look further for a solution in the future for now there's still some 'manual' work needed\n"
-      "-Matthew Brian Darmadi")
-
-print("\npress 'enter' to exit...")
-input()
+print("developed by Matthew Brian Darmadi, thank you.")
